219052cc2e83861fc427a572105af1aa0d4009ef
abeljosetoms115 <abeljosetoms115@hotmail.com>
2021-11-10 21:08:17 +0100

    FEAT: adicionado projecto de teste

---
+++ IspcaNotas.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IspcaNotas.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IspcaNotas.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dca90846-26f3-4a8c-9ccb-fc6136b75cf7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ IspcaNotas.Test/TestLogin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IspcaNotas.Features;
+using IspcaNotas.Model;
+using IspcaNotas.ViewModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace IspcaNotas.Test
+{
+    [TestClass]
+    public class TestLogin
+    {
+        public TestContext TestContext { get; set; }
+        [TestInitialize]
+        public void BeforeAll()
+        {
+
+        }
+        [TestCleanup]
+        public void EndAll()
+        {
+
+        }
+    }
+}
