67b6da3a189c0bc6252e1445f08fc0506f13288d
abeljosetoms115 <abeljosetoms115@hotmail.com>
2021-11-10 21:59:23 +0100

    FEAT: add model usuario

---
+++ IspcaNotas/IspcaNotas/Model/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IspcaNotas.Model
+{
+    public class Usuario
+    {
+        public int Key { get; set; }
+        public int Displayname { get; set; }
+        public int Email { get; set; }
+        public int Senha { get; set; }
+        public int Telefone { get; set; }
+    }
+}
