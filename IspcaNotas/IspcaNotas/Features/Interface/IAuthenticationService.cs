e932e9f892545a4d1b7180ccf59b62764bb7ee3f
abeljosetoms115 <abeljosetoms115@hotmail.com>
2021-11-09 21:36:14 -0800

    FEAT: adicionada interface e view model do login

--- IspcaNotas/IspcaNotas/Features/Interface/IAuthenticationService.cs
+++ IspcaNotas/IspcaNotas/Features/Interface/IAuthenticationService.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace IspcaNotas.Features.Service
+namespace IspcaNotas.Features.Interface
 {
     public interface IAuthenticationService
     {
---
+++ IspcaNotas/IspcaNotas/ViewModel/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using IspcaNotas.Features.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace IspcaNotas.ViewModel
+{
+    public class LoginViewModel
+    {
+    }
+}
