09c02366b11e52aaff9f5b4c11568c60ecaffa85
Abel.Tomas.16 <abeljosetoms115@gmail.com>
2021-12-07 01:34:02 +0100

    FEAT: finalizando a aplicação

--- IspcaNotas/IspcaNotas.Android/MainActivity.cs
+++ IspcaNotas/IspcaNotas.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace IspcaNotas.Droid
 {
-    [Activity(Label = "IspcaNotas", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Notas", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
